6750d16d328b64e25ec963fbc25e7bfbe15c6989
Ameya Ranade <62432771+Ameya1104@users.noreply.github.com>
2023-03-21 23:51:57 +0530

    file upload

--- photoapp/app.js
+++ photoapp/app.js
@@ -5,48 +5,67 @@ const express = require("express"),
 const multer = require('multer');
 const sharp = require('sharp');
 const upload = multer({ dest: 'uploads/' });
-const admin = require('firebase-admin');
-
-var serviceAccount = require('./admin.json');
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://project-id.firebaseio.com",
-    authDomain: "adbms-expt.firebaseapp.com",
-});
-
-var db = admin.database();
-var userRef = db.ref("users");
+const admin = require('firebase-admin')
 
 // Set up view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+var serviceAccount = require('./admin.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://adbms-expt-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    authDomain: "adbms-expt.firebaseapp.com",
+    storageBucket: 'gs://adbms-expt.appspot.com'
+});
 
 // Render upload form
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+// Set up route to handle POST request from form submission
+app.post('/upload', upload.single('image'), function (req, res, next) {
+    const file = req.file;
+    if (!file) {
+      const error = new Error('Please upload a file');
+      error.status = 400;
+      return next(error);
+    }
+    // Upload file to Firebase Storage
+    const bucket = admin.storage().bucket();
+    const timestamp = new Date().getTime().toString();
+    const fileName = `${timestamp}-${file.originalname}`;
+    const fileRef = bucket.file(fileName);
+    const stream = fileRef.createWriteStream({
+      metadata: {
+        contentType: file.mimetype
+      }
+    });
+    stream.on('error', function (error) {
+      console.log('Error uploading file:', error);
+      next(error);
+    });
+    stream.on('finish', function () {
+      // Add file information to Firebase Realtime Database
+      const db = admin.database();
+      const ref = db.ref('uploads');
+      ref.push({
+        filename: fileName,
+        contentType: file.mimetype,
+        size: file.size
+      }, function (error) {
+        if (error) {
+          console.log('Error adding file to database:', error);
+          next(error);
+        } else {
+          res.send('File uploaded successfully');
+        }
+      });
+    });
+    stream.end(req.file.buffer);
+  });
+
 app.listen(3000, function () {
     console.log("App has started!!!");
-});
-
-app.post('/upload', upload.single('image'), async (req, res) => {
-    const { path, mimetype } = req.file;
-
-    // Check if uploaded file is an image
-    if (!mimetype.startsWith('image/')) {
-        return res.status(400).json({ error: 'Only images are allowed' });
-    }
-
-    try {
-        // Resize image to 200x200 pixels
-        const buffer = await sharp(path).resize(200, 200).toBuffer();
-
-        // TODO: Save buffer to storage (e.g. AWS S3, Google Cloud Storage)
-
-        res.json({ message: 'Image uploaded and processed successfully' });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Something went wrong' });
-    }
 });
