cb341bd9b7a04d7d4848e2ee897e715fdc9c33e7
Ameya Ranade <62432771+Ameya1104@users.noreply.github.com>
2023-03-22 08:16:34 +0530

    resizing

--- photoapp/app.js
+++ photoapp/app.js
@@ -24,6 +24,10 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/success', (req, res) => {
+    res.render('success');
+  });
+
 // Set up route to handle POST request from form submission
 app.post('/upload', upload.single('image'), function (req, res, next) {
     const file = req.file;
@@ -59,12 +63,57 @@ app.post('/upload', upload.single('image'), function (req, res, next) {
           console.log('Error adding file to database:', error);
           next(error);
         } else {
-          res.send('File uploaded successfully');
+            res.render('success', { fileName: fileName });
         }
       });
     });
     stream.end(req.file.buffer);
   });
+
+// Set up route to handle POST request to resize image
+app.post('/resize', function (req, res, next) {
+    const bucket = admin.storage().bucket();
+    const timestamp = new Date().getTime().toString();
+    const fileName = req.body.fileName;
+    const resizedFileName = `resized-${timestamp}-${fileName}`;
+  
+    // Download original image from Firebase Storage
+    const fileRef = bucket.file(fileName);
+    fileRef.download(function (error, contents) {
+      if (error) {
+        console.log('Error downloading file:', error);
+        next(error);
+      } else {
+        // Resize image using sharp library
+        sharp(contents)
+          .resize(200, 200)
+          .toBuffer(function (error, data) {
+            if (error) {
+              console.log('Error resizing image:', error);
+              next(error);
+            } else {
+              // Upload resized image to Firebase Storage
+              const resizedFileRef = bucket.file(resizedFileName);
+              const stream = resizedFileRef.createWriteStream({
+                metadata: {
+                  contentType: file.mimetype
+                }
+              });
+              stream.on('error', function (error) {
+                console.log('Error uploading file:', error);
+                next(error);
+              });
+              stream.on('finish', function () {
+                console.log('Image resized successfully');
+                res.render('resize', {bucket: bucket, file: resizedFileRef })
+              });
+              stream.end(data);
+            }
+          });
+      }
+    });
+  });
+ 
 
 app.listen(3000, function () {
     console.log("App has started!!!");
